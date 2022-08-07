a91bf7b7c576f6939934c80aa594f2b2ffeb3fed
Mandar Prakash Kulkarni <mandar4567@gmail.com>
2022-08-08 01:26:52 +0530

    Adding all files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    colors: {
+      'back' :'hsl(0, 15%, 8%)',
+    },
+  },
+  extend: {
+      
+  },
+  plugins: [],
+}
